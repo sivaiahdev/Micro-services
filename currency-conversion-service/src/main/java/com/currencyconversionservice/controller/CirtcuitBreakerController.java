2c7180c91e2c39868b11f9330d14cf7aec3bee67
Siavaiah Devarakonda <sivaiah.dev@gamil.com>
2023-03-25 21:01:57 +0530

    new commit

--- currency-conversion-service/src/main/java/com/currencyconversionservice/controller/CirtcuitBreakerController.java
+++ currency-conversion-service/src/main/java/com/currencyconversionservice/controller/CirtcuitBreakerController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CirtcuitBreakerController {
